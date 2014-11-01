c1ece79a581c5121b282b35caf3dd421dc1b49ed
sanchousese <saniasutula@gmail.com>
2014-11-01 23:23:27 +0200

    CountingSort

--- Sutula/src/CountingSort/CountingSort.java
+++ Sutula/src/CountingSort/CountingSort.java
@@ -25,23 +25,6 @@ public class CountingSort {
             freq.put(e, freq.get(e) + 1);
         }
     }
-//
-//    private static <E extends Comparable> Map<E, Integer> sort(Map<E, Integer> freq) {
-//        List<Map.Entry<E, Integer>> list = new LinkedList<>(freq.entrySet());
-//        Collections.sort(list, new Comparator<Map.Entry<E, Integer>>() {
-//            @Override
-//            public int compare(Map.Entry<E, Integer> o1, Map.Entry<E, Integer> o2) {
-//                return (o1.getValue()).compareTo(o2.getValue());
-//            }
-//        });
-//
-//        Map<E, Integer> map = new LinkedHashMap<>();
-//        for (Iterator it = list.iterator(); it.hasNext();){
-//            Map.Entry<E, Integer> entry = (Map.Entry<E, Integer>) it.next();
-//            map.put(entry.getKey(), entry.getValue());
-//        }
-//        return map;
-//    }
 
 
     public static void main(String[] args) {
