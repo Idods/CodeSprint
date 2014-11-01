299dcb5c99a34ea9c9de91b85725ba3efe46071a
sanchousese <saniasutula@gmail.com>
2014-11-02 01:15:26 +0200

    QuickSort

---
+++ Sutula/src/QuickSort/QuickSort.java
@@ -0,0 +1,46 @@
+package QuickSort;
+
+import java.util.Arrays;
+
+/**
+ * Created by sancho on 01.11.14.
+ */
+public class QuickSort {
+    private static <E extends Comparable> int partition(E[] mas, int left, int right) {
+        E median = mas[(right + left) / 2];
+        int l = left;
+        int r = right;
+
+        while (l <= r) {
+            while (mas[l].compareTo(median) > 0)
+                l++;
+            while (mas[r].compareTo(median) < 0)
+                r--;
+            if (l <= r) {
+                E tmp = mas[r];
+                mas[r] = mas[l];
+                mas[l] = tmp;
+                l++;                //Без цих двох рядків ніфіга не працює
+                r--;
+            }
+        }
+
+        return l;
+    }
+
+    public static <E extends Comparable> void sort(E[] mas, int left, int right) {
+        int i = partition(mas, left, right);
+        if (left < i - 1)
+            sort(mas, left, i - 1);
+        if (i < right)
+            sort(mas, i, right);
+    }
+
+
+    public static void main(String[] args) {
+        Integer[] mas = {1, 12, 5, 26, 7, 14, 3, 7, 2};
+        sort(mas, 0, mas.length - 1);
+        System.out.println(Arrays.toString(mas));
+
+    }
+}
