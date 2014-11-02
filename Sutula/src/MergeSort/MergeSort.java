578b23d7aa0c65e96a5abff437c62c72b947c30c
sanchousese <saniasutula@gmail.com>
2014-11-02 12:06:14 +0200

    MergeSort

---
+++ Sutula/src/MergeSort/MergeSort.java
@@ -0,0 +1,48 @@
+package MergeSort;
+
+import java.util.Arrays;
+
+/**
+ * Created by sancho on 02.11.14.
+ */
+public class MergeSort {
+
+    public static <E extends Comparable> void sort(E[] mas, int left, int right) {
+        if (right - left == 0)
+            return;
+        sort(mas, left, (right + left) / 2);
+        sort(mas, (right + left) / 2 + 1, right);
+
+        int currFirstMas = left;
+        int currSecondMas = (right + left) / 2 + 1;
+
+        E[] merged = (E[]) new Comparable[right - left + 1];
+
+        int currIndex = 0;
+
+        while (currFirstMas <= (right + left) / 2 && currSecondMas <= right) {
+            merged[currIndex] = mas[currFirstMas].compareTo(mas[currSecondMas]) < 0
+                    ? mas[currFirstMas++] : mas[currSecondMas++];
+            currIndex++;
+        }
+
+        while (currIndex < merged.length)
+            if (currFirstMas <= (right + left) / 2) {
+                merged[currIndex++] = mas[currFirstMas++];
+            } else {
+                merged[currIndex++] = mas[currSecondMas++];
+            }
+
+        for (int i = 0; i < merged.length; i++)
+            mas[left + i] = merged[i];
+
+    }
+
+    public static void main(String[] args) {
+        Integer[] mas = {45, 23, 11, 89, 77, 98, 4, 28, 65, 43};
+
+        sort(mas, 0, mas.length - 1);
+
+        System.out.println(Arrays.toString(mas));
+    }
+}
